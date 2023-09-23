fe7371325a60c95f4b5f8ec3b76348541cc99140
Marta Lopes [1221850] <1221850@isep.ipp.pt>
2023-09-24 00:59:57 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+echo "Hello World!"
+?>
+
+<?php echo file_get_contents("html/header.html"); ?>
+<?php echo file_get_contents("html/body.html"); ?>
+<?php echo file_get_contents("html/footer.html"); ?>
+
+<p>Copyright &copy; Marlo <?php echo date("Y"); ?></p>
